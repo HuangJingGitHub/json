e167e967813ab8962d78f2364a0291dc3148b4e8
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2018-06-30 13:16:24 +0800

    6.30

--- leptjson.c
+++ leptjson.c
@@ -1,5 +1,7 @@
 #include "leptjson.h"
 #include <assert.h>
+#include <errno.h>
+#include <math.h> 
 #include <stdlib.h>
 
 #define EXPECT(c, ch)   do{ assert(*c->json == (ch)); c->json++; } while(0)
